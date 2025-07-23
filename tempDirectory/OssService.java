6b0e7773be0c2c8f779fa89ceae9f884486f5bf7
realmayor <1714083619@qq.com>
2025-07-23 10:09:40 +0800

    版本6:学生选课系统+ai

--- tempDirectory/OssService.java
+++
@@ -1,8 +0,0 @@
-package com.me.mall.service.system;
-
-import com.me.mall.common.api.CommonResult;
-import org.springframework.web.multipart.MultipartFile;
-
-public interface OssService {
-    CommonResult uploadOss(MultipartFile file, String dir);
-}
